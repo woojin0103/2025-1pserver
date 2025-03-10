9afff57ea0a3ca637a6c961cae6cd5c77afc8727
woojin0103 <ahawow0103@naver.com>
2025-03-10 16:03:04 +0900

    fastAPI 붓꽃분류시스템을 차근차근 만들어보자

--- irisModelBase.py
+++ irisModelBase.py
@@ -1,8 +1,10 @@
 #2025.3.10
 #project2 붓꽃분류기 만들기
 # 이용희 교수님과 열심히 만들어보자
+import joblib
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 
 iris_df = pd.read_csv('iris.csv')
@@ -16,8 +18,18 @@ print(X)
 kn = KNeighborsClassifier()
 model_kn = kn.fit(X,Y)
 
+rfc = RandomForestClassifier()
+model_rfc = rfc.fit(X,Y)
+
+joblib.dump(model_rfc,'model_rfc.pkl')
+
 X_new = np.array([[5,3.4,1.4,0.2]])
 prediction = model_kn.predict(X_new)
+
+prediction = model_kn.predict(X_new)
+prediction = model_rfc.predict(X_new)
 print(prediction)
 probability = model_kn.predict_proba(X_new)
+probability = model_rfc.predict_proba(X_new)
 print(probability)
+
