0fd46531d3ae5642909548d6a50385fc59b92d40
woojin0103 <ahawow0103@naver.com>
2025-03-10 12:18:25 +0900

    KNeihborsClassfiler를 사용한 붓꽃분류

---
+++ irisModelBase.py
@@ -0,0 +1,23 @@
+#2025.3.10
+#project2 붓꽃분류기 만들기
+# 이용희 교수님과 열심히 만들어보자
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+
+iris_df = pd.read_csv('iris.csv')
+print(iris_df)
+
+Y = iris_df['species']
+X = iris_df.drop('species',axis=1)
+print(Y)
+print(X)
+
+kn = KNeighborsClassifier()
+model_kn = kn.fit(X,Y)
+
+X_new = np.array([[5,3.4,1.4,0.2]])
+prediction = model_kn.predict(X_new)
+print(prediction)
+probability = model_kn.predict_proba(X_new)
+print(probability)
